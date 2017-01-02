f13f7909e9016262eafe3be8e1d2b5714a956323
SMatherson <matherson@outlook.com.au>
2017-01-02 18:44:44 +1030

    Add files via upload

---
+++ ConfigureNetworks.ps1
@@ -0,0 +1,20 @@
+﻿#NETWORK-RECONFIG
+# ****************REMOVES DHCP & SETS STATIC IP**********
+
+Get-NetIPInterface
+#************************************************
+
+$IFIndex=Read-Host -Prompt "Enter Interface Index You would like to Configure from the above"
+
+$DesiredIPAddress=Read-Host -Prompt "Please Enter An IP Address To Configure"
+
+$IPPrefix=Read-Host -Prompt "Enter an IPv4 Subnet Prefix"
+
+$Gateway=Read-Host -Prompt "Enter Default Gateway Address"
+
+$DNS =Read-Host -Prompt "Enter DNS Server Address"
+
+Set-NetIPInterface -Dhcp Disabled
+New-NetIPAddress -IPAddress $DesiredIPAddress -InterfaceIndex $IFIndex -PrefixLength $IPPrefix -DefaultGateway $Gateway
+Set-NetConnectionProfile -InterfaceIndex $IFIndex -NetworkCategory Private
+Set-DnsClientServerAddress -InterfaceIndex $IFIndex -ServerAddresses $DNS
